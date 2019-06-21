df7cd5b1c07a14824ea993fa54052878a115997f
pankajan05 <pankajan05@gmail.com>
2019-06-21 18:13:01 +0530

    write the basic program and the assingment to print details

---
+++ first program/MYPROJEC.C
@@ -0,0 +1,30 @@
+/*
+Author: Pankajan  S
+Purpose:Print the Student details
+Date Created: 23/05/2018
+Date Modified: 23/05/2018
+ */
+
+//preprocessor way to include header file
+#include<stdio.h>
+#include<conio.h>
+
+//This is main function
+void main()
+//start the main function
+{
+
+//use to clear the before the printing in screen
+clrscr();
+
+//command to print the screen
+printf("Name: Pankajan   S.\n");
+printf("Student number: SE\\2016\\032\n");
+printf("Age: 21\n");
+printf("Address:Earlalai East,\n\tEarlalai,\n\tJaffna.\n");
+
+//use to hold thwe screen
+getch();
+
+//end of the main function
+}
---
+++ first program/PANKAJAN.C
@@ -0,0 +1,23 @@
+/*My first programme
+This programme use to print a single line */
+
+//click f10 and select the combo to repaint for remove the window struct
+
+//preprocessor way to include header file
+#include<stdio.h>
+#include<conio.h>
+
+//This is main function
+void main()
+//start the main function
+{
+//use to clear the before the printing in screen
+clrscr();
+//command to print the screen
+printf("Welcome Pankajan to"
+	" C programming.\n");//indicate the command finish
+
+//use to hold thwe screen
+getch();
+//end of the main function
+}
